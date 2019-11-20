aa231fe77902fa5759aa3a7a88bf06def62f30b6
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-20 12:08:52 +0000

    first failing test

--- spec/units/todo_list_spec.rb
+++ spec/units/todo_list_spec.rb
@@ -1,8 +1,10 @@
 require 'todolist'
+
 describe ToDoList do
 
  it 'add something into the todolist' do
-   todolist = ToDoList.new
+   todolist = ToDoList.new(todo_item)
+   expect(todo_list).to include(todo_item)
  end
 
  it 'read all the items in the todo list' do
