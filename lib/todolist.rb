402635c478fbf3b9104b88547b7171c4d9c6d33d
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-21 11:10:24 +0000

    failing unit tests

--- lib/todolist.rb
+++ lib/todolist.rb
@@ -1,5 +1,12 @@
 #TODO - test drive this model object to have
 
 class ToDoList
+def add_item(item)
+  todolist = [ ]
+  todolist << item
+end
 
+def list_todos
+  todolist.print
+end
 end
--- spec/units/todo_list_spec.rb
+++ spec/units/todo_list_spec.rb
@@ -2,12 +2,13 @@ require 'todolist'
 
 describe ToDoList do
 
- it 'add something into the todolist' do
-   todolist = ToDoList.new(todo_item)
-   expect(todo_list).to include(todo_item)
+ it 'adds something into the todolist' do
+   todolist = ToDoList.new
+   expect(todo_list).to include(item)
  end
 
- it 'read all the items in the todo list' do
+ it 'reads all the items in the todo list' do
    todolist = ToDoList.new
+   expect(todo_list).to include(item)
  end
 end
